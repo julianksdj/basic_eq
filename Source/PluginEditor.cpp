ea8844c7d9693ae80e369540dbfc903a056db334
juliksdj <julian.ksdj@gmail.com>
2024-01-24 22:52:03 +0100

    8 band gui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,13 +9,38 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+#define MARGIN 10
+#define BAND_W 40
+#define SLIDER_H 300
+#define LABEL_W BAND_W
+#define LABEL_H 40
+#define BAND_SEPARATION (BAND_W + MARGIN)
+#define LABEL_POS_H ((2 * MARGIN) + SLIDER_H)
+#define WINDOW_W ((NUMBER_OF_BANDS * (BAND_W + MARGIN)) + MARGIN)
+#define WINDOW_H (SLIDER_H + LABEL_H + (2 * MARGIN))
+
 //==============================================================================
-Basic_eqAudioProcessorEditor::Basic_eqAudioProcessorEditor (Basic_eqAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+Basic_eqAudioProcessorEditor::Basic_eqAudioProcessorEditor(Basic_eqAudioProcessor& p)
+    : AudioProcessorEditor(&p), audioProcessor(p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize(WINDOW_W, WINDOW_H);
+
+    for (int i = 0; i < NUMBER_OF_BANDS; i++) {
+        addAndMakeVisible(band_sld[i]);
+        band_sld[i].setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+        band_sld[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, BAND_W, 30);
+        band_sld[i].addListener(this);
+        band_sld[i].setRange(-48.0, 10.0);
+        
+        addAndMakeVisible(band_lbl[i]);
+        juce::String lbl_str = "f" + juce::String(i + 1);
+        band_lbl[i].setText(lbl_str, juce::dontSendNotification);
+        band_lbl[i].setJustificationType(juce::Justification::centred);
+    }
+    
+    
 }
 
 Basic_eqAudioProcessorEditor::~Basic_eqAudioProcessorEditor()
@@ -23,18 +48,32 @@ Basic_eqAudioProcessorEditor::~Basic_eqAudioProcessorEditor()
 }
 
 //==============================================================================
-void Basic_eqAudioProcessorEditor::paint (juce::Graphics& g)
+void Basic_eqAudioProcessorEditor::paint(juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour(juce::Colours::white);
+    g.setFont(15.0f);
 }
 
 void Basic_eqAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    int x = 10;
+    int y = 10;
+    for (int i = 0; i < NUMBER_OF_BANDS; i++) {
+        band_sld[i].setBounds(x, MARGIN, BAND_W, SLIDER_H);
+        band_lbl[i].setBounds(x, LABEL_POS_H, LABEL_W, LABEL_H);
+        x += BAND_SEPARATION;
+    }
+}
+
+void Basic_eqAudioProcessorEditor::sliderValueChanged(juce::Slider *slider)
+{
+    for (int i = 0; i < NUMBER_OF_BANDS; i++) {
+        if(slider == &band_sld[i])
+        {
+            DBG("band_sld[" << i << "]="<< band_sld[i].getValue());
+        }
+    }
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,10 +11,13 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+#define NUMBER_OF_BANDS 8
+
 //==============================================================================
 /**
 */
-class Basic_eqAudioProcessorEditor  : public juce::AudioProcessorEditor
+class Basic_eqAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                      public juce::Slider::Listener
 {
 public:
     Basic_eqAudioProcessorEditor (Basic_eqAudioProcessor&);
@@ -23,11 +26,20 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+        
+    // Controls
+    void sliderValueChanged (juce::Slider *slider) override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Basic_eqAudioProcessor& audioProcessor;
+    
+    // Controls
+    juce::Slider band_sld[NUMBER_OF_BANDS];
+    
+    // Labels
+    juce::Label band_lbl[NUMBER_OF_BANDS];
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Basic_eqAudioProcessorEditor)
 };
